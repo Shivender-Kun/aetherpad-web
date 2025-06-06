774b6210f77dd0b4d9129ba19fb6b73490f28a01
Shivender-Kun <shivenderkumar761@gmail.com>
2025-06-06 15:01:26 +0530

    Add & Edit Note Dialog updated; supports card color & pin state

--- src/app/(authorized)/home/_edit/index.tsx
+++ src/app/(authorized)/home/_edit/index.tsx
@@ -11,6 +11,7 @@ import {
   FormControl,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { updateNoteSchema } from "@/validations/notes.validation";
@@ -24,6 +25,14 @@ import { useForm } from "react-hook-form";
 import { useStore } from "@/store";
 import { INote } from "@/types";
 import z from "zod";
+import { useState } from "react";
+import { PALETTE_OPTIONS } from "@/constants";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Palette, Pin, PinOff, Tags } from "lucide-react";
 
 const EditNote = ({
   selectedNote,
@@ -33,6 +42,10 @@ const EditNote = ({
   closeDialog: () => void;
 }) => {
   const { labels, setAPIMessage } = useStore();
+  const [noteAttributes, setNoteAttributes] = useState(() => ({
+    bgColor: selectedNote.bgColor || "",
+    isPinned: selectedNote.isPinned || false,
+  }));
 
   // Convert labelList to the format required by MultiSelect
   const labelsOptions = labels.list.map((label) => ({
@@ -49,97 +62,166 @@ const EditNote = ({
     },
   });
 
-  const onSubmit = async (data: z.infer<typeof updateNoteSchema>) => {
-    console.log(data);
+  const HeaderOptions = (
+    <Popover>
+      <PopoverTrigger
+        className="absolute top-4 right-4 rounded-full w-7 h-7 cursor-pointer"
+        title="Pin"
+        onClick={(e) => {
+          e.stopPropagation();
+          setNoteAttributes((prev) => ({ ...prev, isPinned: !prev.isPinned }));
+        }}
+      >
+        {noteAttributes.isPinned ? <PinOff /> : <Pin />}
+      </PopoverTrigger>
+    </Popover>
+  );
 
-    // noteAction({
-    //   id: selectedNote._id,
-    //   action: "UPDATE",
-    //   data,
-    //   showToast: setAPIMessage,
-    // });
-    // closeDialog();
-    // form.reset();
+  const renderPaletteOption = (
+    <Popover>
+      <PopoverTrigger
+        title="Card Color"
+        className="rounded-full w-7 h-7 cursor-pointer "
+      >
+        <Palette />
+      </PopoverTrigger>
+      <PopoverContent
+        className="w-full"
+        onClick={(e) => {
+          const target = e.target as HTMLDivElement;
+          const role = target.role;
+          if (role === "button") {
+            const bgColor = target.dataset.bgColor;
+            console.log("Background Color:", bgColor);
+
+            if (bgColor) setNoteAttributes((prev) => ({ ...prev, bgColor }));
+          }
+        }}
+      >
+        <div className="flex gap-4">
+          {PALETTE_OPTIONS.map((color) => (
+            <div
+              key={color}
+              role="button"
+              data-bg-color={color}
+              aria-label={color}
+              className={`w-4 h-4 rounded-full cursor-pointer border`}
+              style={{ backgroundColor: `var(--color-${color})` }}
+            ></div>
+          ))}
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+
+  const renderLabelsOption = (
+    <Popover>
+      <PopoverTrigger
+        title="Labels"
+        className="rounded-full w-7 h-7 cursor-pointer "
+      >
+        <Tags />
+      </PopoverTrigger>
+      <PopoverContent className="w-full" onClick={() => {}}>
+        <FormField
+          control={form.control}
+          name="labels"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="labels" className="max-w-fit">
+                Labels
+              </FormLabel>
+              <FormControl>
+                <MultiSelect
+                  id="labels"
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  options={labelsOptions}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+
+  const onSubmit = async (data: z.infer<typeof updateNoteSchema>) => {
+    noteAction({
+      id: selectedNote._id,
+      action: "UPDATE",
+      data: { ...data, ...noteAttributes },
+      showToast: setAPIMessage,
+    });
+
+    closeDialog();
+    form.reset();
   };
 
   return (
     <DialogContent
+      className="[&>button]:hidden"
       style={{
         backgroundColor: `var(--${
-          selectedNote.bgColor ? `color-${selectedNote.bgColor}` : "card"
+          noteAttributes.bgColor ? `color-${noteAttributes.bgColor}` : "card"
         })`,
       }}
     >
       <DialogHeader>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-4"
-          >
-            <DialogTitle className="pr-5">
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        id="title"
-                        {...field}
-                        placeholder="Title"
-                        className="bg-inherit dark:bg-inherit border-0"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </DialogTitle>
-
-            <FormField
-              control={form.control}
-              name="content"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      id="content"
-                      {...field}
-                      className="h-64 resize-none bg-inherit dark:bg-inherit border-0"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="labels"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <MultiSelect
-                      id="labels"
-                      onValueChange={field.onChange}
-                      options={labelsOptions}
-                      placeholder="Labels"
-                      className="bg-inherit dark:bg-inherit border-0"
-                      defaultValue={field.value}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <DialogFooter>
-              <Button>Save</Button>
-            </DialogFooter>
-          </form>
-        </Form>
+        <DialogTitle className="pr-5">Edit Note</DialogTitle>
+        {HeaderOptions}
       </DialogHeader>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="title">Title</FormLabel>
+                <FormControl>
+                  <Input id="title" placeholder="Title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="content">Content</FormLabel>
+                <FormControl>
+                  <Textarea
+                    id="content"
+                    className="h-64 resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <DialogFooter>
+            <div className="flex gap-4 justify-between w-full">
+              <div className="flex gap-4">
+                {renderPaletteOption}
+                {renderLabelsOption}
+              </div>
+            </div>
+            <Button>Save</Button>
+          </DialogFooter>
+        </form>
+      </Form>
     </DialogContent>
   );
 };
