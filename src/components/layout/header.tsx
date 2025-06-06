8d78c109fbbe5c4301aad9c42b2f24b12caf4883
Shivender-Kun <shivenderkumar761@gmail.com>
2025-06-06 10:37:34 +0530

    mobile sidebar opener added

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { ThemeModeToggle } from "../Theme/theme-mode-toggle";
 import { NotebookPen } from "lucide-react";
+import { SidebarTrigger } from "../ui/sidebar";
 
 const Header = () => {
   return (
     <header className="border-y flex justify-between items-center p-4 gap-4">
+      <div className="md:hidden">
+        <SidebarTrigger />
+      </div>
       <div className="flex justify-center items-center gap-4">
         <NotebookPen />
         <h1 className="text-2xl font-bold text-center">Personal Notes</h1>
