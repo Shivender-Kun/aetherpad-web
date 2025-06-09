8b55f041855305b7eea68c616884e62ae9d98eec
Shivender-Kun <shivenderkumar761@gmail.com>
2025-06-09 22:58:48 +0530

    user validation schema updated

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -5,8 +5,12 @@ const registerUserSchema = z
     email: z.string().email(),
     password: z
       .string()
-      .min(6, "Password must be at least 6 characters long")
-      .max(50, "Password must be at most 50 characters long"),
+      .min(6, "Password must be at least 6 characters")
+      .max(30, "Password must be at most 50 characters")
+      .regex(
+        /^[a-zA-Z0-9]{6,30}$/,
+        "Username must contain only letters and numbers"
+      ),
     confirmPassword: z.string(),
     profilePicture: z
       .any()
@@ -18,38 +22,55 @@ const registerUserSchema = z
   });
 
 const loginSchema = z.object({
+  password: z.string(),
   email: z.string().email(),
-  password: z
-    .string()
-    .min(6, "Password must be at least 6 characters long")
-    .max(50, "Password must be at most 50 characters long"),
 });
 
 const updateUserSchema = z.object({
-  profilePicture: z.string(),
-  coverPicture: z.string(),
   username: z.string(),
+  coverPicture: z.string(),
+  profilePicture: z.string(),
 });
 
 const changePasswordSchema = z.object({
-  oldPassword: z
-    .string()
-    .min(6, "Password must be at least 6 characters long")
-    .max(50, "Password must be at most 50 characters long"),
+  oldPassword: z.string(),
   newPassword: z
     .string()
-    .min(6, "Password must be at least 6 characters long")
-    .max(50, "Password must be at most 50 characters long"),
+    .min(6, "Password must be at least 6 characters")
+    .max(30, "Password must be at most 50 characters")
+    .regex(
+      /^[a-zA-Z0-9]{6,30}$/,
+      "Username must contain only letters and numbers"
+    ),
 });
 
 const forgotPasswordSchema = z.object({
   email: z.string().email("Invalid email address"),
 });
 
+const resetPasswordSchema = z
+  .object({
+    token: z.string(),
+    confirmPassword: z.string(),
+    newPassword: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .max(30, "Password must be at most 50 characters")
+      .regex(
+        /^[a-zA-Z0-9]{6,30}$/,
+        "Username must contain only letters and numbers"
+      ),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
 export {
-  registerUserSchema,
   loginSchema,
   updateUserSchema,
+  registerUserSchema,
+  resetPasswordSchema,
   changePasswordSchema,
   forgotPasswordSchema,
 };
