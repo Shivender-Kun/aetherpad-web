a1ca9b5d25462ecf447155e91eeec4dadb8d2b18
Shivender-Kun <shivenderkumar761@gmail.com>
2025-06-14 10:38:56 +0530

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -57,3 +56,5 @@ const nextConfig: NextConfig = {
     ];
   },
 };
+
+export default nextConfig;
