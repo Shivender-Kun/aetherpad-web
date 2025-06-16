e66d3ac9843771b512ae9faa14c25c919830acba
Shivender-Kun <shivenderkumar761@gmail.com>
2025-06-16 13:05:57 +0530

    global error page updated

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,22 +1,25 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-const Error = ({}: {
-  error: Error & { digest?: string };
+const Error = ({
+  reset,
+}: {
   reset: () => void;
+  error: Error & { digest?: string };
 }) => {
-  const route = useRouter();
-
-  useEffect(() => {
-    setTimeout(() => route.replace("/login"), 1000);
-  }, [route]);
-
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-full">
       <h2 className="font-medium text-3xl">Something went wrong!</h2>
-      <p className="text-xl">Redirecting to login page...</p>
+
+      <div className="flex gap-4">
+        <Link href="/login">
+          <Button>Go To Login Page </Button>
+        </Link>
+
+        <Button onClick={reset}>Retry</Button>
+      </div>
     </div>
   );
 };
